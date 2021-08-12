db0ccf785a8d912d293b0f55911db7c1c71087de
Elijahm124 <elijahmurillo124@gmail.com>
2021-08-12 16:35:54 -0400

    Users app functionality added

---
+++ fits/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'fits.apps.FitsConfig'
--- fits/apps.py
+++ fits/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FitsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fits'
+
+    def ready(self):
+        import fits.signals
